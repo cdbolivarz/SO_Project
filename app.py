1d839f631965a8ed4be8494dd1eab44168659c45
Darwin Stiven Herrera Cartagena <dsherrerac@unal.edu.co>
2019-08-03 22:39:40 -0500

    Un poco de flask

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home_func():
     return render_template("home.html")
+@app.route('/pagina')
+def indexpag():
+    return "Vista pagina web"
+
+@app.route('/carpetas')
+def indexdoc():
+    return "Vista carpetas actuales"
